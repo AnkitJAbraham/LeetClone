9fd8ef3acd08a90af70a62c08b8dfcb41e35f5c3
Ankit-John <ankitjohn008@gmail.com>
2023-08-29 22:17:37 +0530

    First commit

---
+++ src/components/Footbar/Footbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-black text-white py-4 sticky bottom-0">
+      <div className="container mx-auto flex justify-start items-center">
+        <p className="text-base">Developed by Ankit John Abraham</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
